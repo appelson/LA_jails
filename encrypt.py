4c9c0bb2be5773acf267786c5558af256e70b43e
Elijah Appelson <124807267+appelson@users.noreply.github.com>
2026-03-16 21:04:26 -0400

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -6,7 +6,7 @@ from cryptography.hazmat.primitives import serialization
 private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 
 # Saving private key
-with open("/Users/eljahappelson/Desktop/private_key.pem", "wb") as f:
+with open("PATH", "wb") as f:
     f.write(private_key.private_bytes(
         serialization.Encoding.PEM,
         serialization.PrivateFormat.PKCS8,
@@ -14,7 +14,7 @@ with open("/Users/eljahappelson/Desktop/private_key.pem", "wb") as f:
     ))
 
 # Saving public key
-with open("/Users/eljahappelson/Desktop/public_key.pem", "wb") as f:
+with open("PATH", "wb") as f:
     f.write(private_key.public_key().public_bytes(
         serialization.Encoding.PEM,
         serialization.PublicFormat.SubjectPublicKeyInfo
